c15c0b5ebfba4bf69e08191f8b0e86f53e07dd78
palat2000 <palat.tungsantitham@gmail.com>
2023-10-10 17:27:20 +0700

    update error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,4 +1,4 @@
 module.exports = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ message: err.message });
+  res.status(err.statusCode || 500).json({ message: err.message });
 };
