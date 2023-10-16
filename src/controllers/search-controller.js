172bd24dbf916214b875f679bdac39a1cf6262ee
unknown <palat.tungsantitham@gmail.com>
2023-10-16 21:01:44 +0700

    getplace

--- src/controllers/search-controller.js
+++ src/controllers/search-controller.js
@@ -1,6 +1,24 @@
+const prisma = require("../model/prisma");
+
 exports.getPlace = async (req, res, next) => {
   try {
-    res.json(req.query);
+    const places = await prisma.placer.findMany({
+      where: {
+        type: req.query.type,
+      },
+      select: {
+        email: true,
+        id: true,
+        imagePlaces: true,
+        lat: true,
+        lng: true,
+        mobile: true,
+        name: true,
+        type: true,
+      },
+    });
+    console.log(places);
+    res.status(200).json({ places });
   } catch (err) {
     next(err);
   }
